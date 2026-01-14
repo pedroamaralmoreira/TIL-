2abf93643916922f2520b99885f5f48f0b28b24b
Pedro Henrique Amaral Moreira <pedrohenriquebf3@gmail.com>
2026-01-14 16:13:38 -0300

    FIZ A ATIVIDADE PAUSADAMENTE PARA ENTENDER OS CONCEITOS

--- IdeaProjects/caderno udemy/src/folhas/section_8_75/exercise2_8_75.java
+++ IdeaProjects/caderno udemy/src/folhas/section_8_75/exercise2_8_75.java
@@ -1,20 +1,57 @@
+package folhas.section_8_75;
 import java.util.Locale;
 import java.util.Scanner;
 
-public  class exercise2_8_75 {
-  String name;
-  double GrossSalary;
-  double tax;
-  double NetSalary(){
-      return(GrossSalary-tax);
-    };
+//metodos.
+public class exercise2_8_75{
+    public static void main (String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee emp = new Employee();
+        System.out.print("Name: ");
+        emp.name=sc.nextLine();
+
+        System.out.print("Gross salary: ");
+        emp.GrossSalary=sc.nextDouble();
+
+        System.out.print("Tax: ");
+        emp.tax = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Employee: " + emp);
+
+        System.out.println();
+        System.out.print("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        emp.increaseSalary(percentage);
+
+        System.out.println("Updated data: " + emp);
+        sc.close();
+
+    }
+
 }
 
-public static void main (String[] args){
-    Locale.setDefault(Locale.US);
-    Scanner sc = new Scanner(System.in);
+//atributos
 
-    company infos = new company();
+class Employee {
 
+    String name;
+    double GrossSalary;
+    double tax;
+
+    double NetSalary() {
+        return (GrossSalary - tax);
+    }
+
+    void increaseSalary(double percentage) {
+        GrossSalary += GrossSalary * percentage / 100.0;
+    }
+    public String toString() {
+        return name + " , $ " + NetSalary();
+    }
 
 }
+
+
